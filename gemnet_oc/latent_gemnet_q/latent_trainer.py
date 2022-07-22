55f80a717ff7a11d0e86b151006c6012a7908e04
jmusiel <73840022+jmusiel@users.noreply.github.com>
2022-07-22 14:49:13 +0000

    add latent trainer and default configs for gemnet-oc running main.py

---
+++ gemnet_oc/latent_gemnet_q/latent_trainer.py
@@ -0,0 +1,159 @@
+import logging
+import os
+import pathlib
+from collections import defaultdict
+from pathlib import Path
+
+import numpy as np
+import torch
+import torch_geometric
+from tqdm import tqdm
+
+from ocpmodels.common import distutils
+from ocpmodels.common.relaxation.ml_relaxation import ml_relax
+from ocpmodels.common.utils import check_traj_files
+from ocpmodels.modules.evaluator import Evaluator
+from ocpmodels.modules.normalizer import Normalizer
+from ocpmodels.trainers.base_trainer import BaseTrainer
+
+from ocpmodels.trainers.forces_trainer import ForcesTrainer
+from ocpmodels.common.registry import registry
+
+@registry.register_trainer("latent")
+class LatentTrainer(ForcesTrainer):
+
+    # Takes in a new data source and generates predictions on it.
+    @torch.no_grad()
+    def predict(
+        self,
+        data_loader,
+        per_image=True,
+        results_file=None,
+        disable_tqdm=False,
+    ):
+        if distutils.is_master() and not disable_tqdm:
+            logging.info("Predicting on test.")
+        assert isinstance(
+            data_loader,
+            (
+                torch.utils.data.dataloader.DataLoader,
+                torch_geometric.data.Batch,
+            ),
+        )
+        rank = distutils.get_rank()
+
+        if isinstance(data_loader, torch_geometric.data.Batch):
+            data_loader = [[data_loader]]
+
+        self.model.eval()
+        if self.ema:
+            self.ema.store()
+            self.ema.copy_to()
+
+        if self.normalizers is not None and "target" in self.normalizers:
+            self.normalizers["target"].to(self.device)
+            self.normalizers["grad_target"].to(self.device)
+
+        predictions = {"id": [], "energy": [], "forces": [], "chunk_idx": [], "latent_h": [], "n_atoms": [], "residual": []}
+
+        for i, batch_list in tqdm(
+            enumerate(data_loader),
+            total=len(data_loader),
+            position=rank,
+            desc="device {}".format(rank),
+            disable=disable_tqdm,
+        ):
+            with torch.cuda.amp.autocast(enabled=self.scaler is not None):
+                out = self._forward(batch_list)
+
+            if self.normalizers is not None and "target" in self.normalizers:
+                out["energy"] = self.normalizers["target"].denorm(
+                    out["energy"]
+                )
+                out["forces"] = self.normalizers["grad_target"].denorm(
+                    out["forces"]
+                )
+            if per_image:
+                systemids = [
+                    str(i) + "_" + str(j)
+                    for i, j in zip(
+                        batch_list[0].sid.tolist(), batch_list[0].fid.tolist()
+                    )
+                ]
+                predictions["id"].extend(systemids)
+                predictions["energy"].extend(
+                    out["energy"].to(torch.float16).tolist()
+                )
+                batch_natoms = torch.cat(
+                    [batch.natoms for batch in batch_list]
+                )
+                batch_fixed = torch.cat([batch.fixed for batch in batch_list])
+                forces = out["forces"].cpu().detach().to(torch.float16)
+                per_image_forces = torch.split(forces, batch_natoms.tolist())
+                per_image_forces = [
+                    force.numpy() for force in per_image_forces
+                ]
+                # evalAI only requires forces on free atoms
+                if results_file is not None:
+                    _per_image_fixed = torch.split(
+                        batch_fixed, batch_natoms.tolist()
+                    )
+                    _per_image_free_forces = [
+                        force[(fixed == 0).tolist()]
+                        for force, fixed in zip(
+                            per_image_forces, _per_image_fixed
+                        )
+                    ]
+                    _chunk_idx = np.array(
+                        [
+                            free_force.shape[0]
+                            for free_force in _per_image_free_forces
+                        ]
+                    )
+                    per_image_forces = _per_image_free_forces
+                    predictions["chunk_idx"].extend(_chunk_idx)
+                predictions["forces"].extend(per_image_forces)
+                predictions["latent_h"].extend(out["latent_h"].tolist())
+                predictions["n_atoms"].extend(batch_natoms.tolist())
+                predictions["residual"].extend(out["energy"].cpu().numpy() - batch_list[0].y.cpu().numpy())
+            else:
+                predictions["energy"] = out["energy"].detach()
+                predictions["forces"] = out["forces"].detach()
+                return predictions
+
+        predictions["forces"] = np.array(predictions["forces"])
+        predictions["chunk_idx"] = np.array(predictions["chunk_idx"])
+        predictions["energy"] = np.array(predictions["energy"])
+        predictions["id"] = np.array(predictions["id"])
+        predictions["latent_h"] = np.array(predictions["latent_h"])
+        self.save_results(
+            predictions, results_file, keys=["energy", "forces", "chunk_idx"]
+        )
+
+        if self.ema:
+            self.ema.restore()
+
+        return predictions
+
+    
+    def _forward(self, batch_list):
+        # forward pass.
+        if self.config["model_attributes"].get("regress_forces", True):
+            out_energy, out_forces = self.model(batch_list)
+        else:
+            out_energy = self.model(batch_list)
+
+        if out_energy.shape[-1] == 1:
+            out_energy = out_energy.view(-1)
+
+        out = {
+            "energy": out_energy,
+        }
+
+        if self.config["model_attributes"].get("regress_forces", True):
+            out["forces"] = out_forces
+
+        out["latent_h"] = self.model.module.latent_h.detach().cpu().numpy()
+        self.model.latent_h = self.model.latent_m = None
+
+        return out
